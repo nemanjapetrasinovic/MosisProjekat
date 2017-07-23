d1d035646c66779b054dfad9a6709ff30f2e061f
nemanjapetrasinovic <nemanjapet94@gmail.com>
2017-07-23 16:21:37 +0200

    Lociranje i prikaz pozicije korisnika na mapi

--- app/src/main/java/com/example/nemanja/mosisprojekat/MainActivity.java
+++ app/src/main/java/com/example/nemanja/mosisprojekat/MainActivity.java
@@ -135,6 +135,8 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            Intent map=new Intent(MainActivity.this.getApplicationContext(),MapsActivity.class);
+            startActivity(map);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
