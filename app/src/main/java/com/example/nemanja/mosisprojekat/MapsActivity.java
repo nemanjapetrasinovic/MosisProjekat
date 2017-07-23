c2a3d389997d86f2ca3d1a94983855c6004b0b21
nemanjapetrasinovic <nemanjapet94@gmail.com>
2017-07-23 18:21:46 +0200

    prikazivanje slicice korisnika na mapi (nije potpuno)

--- app/src/main/java/com/example/nemanja/mosisprojekat/MapsActivity.java
+++ app/src/main/java/com/example/nemanja/mosisprojekat/MapsActivity.java
@@ -3,22 +3,50 @@ package com.example.nemanja.mosisprojekat;
 import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +56,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth=FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
     }
 
 
@@ -45,9 +92,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -55,6 +102,85 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } else {
             // Show rationale and request permission.
         }
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null)
+        {
+            DatabaseReference userRef=mDatabase.child("user").child(user.getUid());
+            userRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    Traveller value = dataSnapshot.getValue(Traveller.class);
+                    String userID=user.getUid();
+                    StorageReference storageReference = storageRef.child(userID+".jpg");
+                    File localFile=null;
+                    try {
+                        localFile = File.createTempFile(userID, "jpg");
+                    }
+                    catch (Exception e){
+
+                    }
+                    final File localfile2=localFile;
+                    storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            // Local temp file has been created
+                            Bitmap myBitmap = BitmapFactory.decodeFile(localfile2.getAbsolutePath());
+                            Bitmap myScaledBitmap=Bitmap.createScaledBitmap(myBitmap,myBitmap.getHeight()/10,myBitmap.getWidth()/10,false);
+                            LatLng sydney = new LatLng(-34, 151);
+                            BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(myScaledBitmap);
+                            MarkerOptions markerOptions = new MarkerOptions().position(sydney)
+                                    .title("Current Location")
+                                    .snippet("Thinking of finding some thing...")
+                                    .icon(icon);
+
+                            Marker mMarker = mMap.addMarker(markerOptions);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle any errors
+                        }
+                    });
+
+                    mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(10, 10))
+                            .title(value.getFirstname()));
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+        }
+
+        /*FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(dataSnapshot!=null)
+            Traveller value = dataSnapshot.getValue(Traveller.class);
+        String userID=user.getUid();
+        StorageReference storageReference = storageRef.child(userID+".jpg");*/
+
+
+        /*final File localFile = File.createTempFile(userID, "jpg");
+
+        storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                // Local temp file has been created
+                Bitmap myBitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                ImageView image=(ImageView) findViewById(R.id.imageView);
+                image.setImageBitmap(myBitmap);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });*/
     }
 
     @Override
--- app/src/main/java/com/example/nemanja/mosisprojekat/Traveller.java
+++ app/src/main/java/com/example/nemanja/mosisprojekat/Traveller.java
@@ -9,4 +9,56 @@ class Traveller {
     public int score;
     public List<String> friends;
     public String picture;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
+
+    public void addFriend(String friend){
+        this.friends.add(friend);
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 }
